3d55610aae0598d913501d689b8e1abdd39fe938
Andrei Bodnar <bodnar1212@gmail.com>
2019-04-22 17:53:01 +0300

    removed author for post meta

--- wp-content/themes/travelify/library/functions/functions.php
+++ wp-content/themes/travelify/library/functions/functions.php
@@ -431,10 +431,7 @@ function travelify_posted_on() {
 		esc_attr( get_the_modified_date( 'c' ) ),
 		esc_html( get_the_modified_date() )
 	);
-	$byline = sprintf(
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-	echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>' . '</span>';
+	echo '<span class="posted-on">' . '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>' . '</span>';
 }
 endif;
 
